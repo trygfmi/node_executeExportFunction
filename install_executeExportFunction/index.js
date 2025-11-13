7e5178761ac8f83bdb2218ace9444cce61e2312e
running-terminal-commands-blog <running-terminal-commands-blog@running-terminal-commands-blog.com>
2025-11-13 23:15:49 +0900

    first commit

---
+++ install_executeExportFunction/index.js
@@ -0,0 +1,4 @@
+console.log("hello npm command world");
+module.exports = () => {
+  console.log("hello export function");
+};
---
+++ workDirectory_executeExportFunction/index.js
@@ -0,0 +1,2 @@
+const pkg = require('install_executeExportFunction');
+pkg();
