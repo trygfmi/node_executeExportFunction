4543b4111cea7d8d79d5cfe0f5b31592d4e390d5
running-terminal-commands <runningterminalcommands@gmail.com>
2025-12-05 02:11:19 +0900

    開発用リポジトリにfirst commit

---
+++ command_example.sh
@@ -0,0 +1,10 @@
+# ./command_example.sh
+
+
+cd work-directory_execute-export-function
+npm init -y
+npm install ../install_execute-export-function
+cat ../install_execute-export-function/index.js
+cat index.js
+node index.js
+
